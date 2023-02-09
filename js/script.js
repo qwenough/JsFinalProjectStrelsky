032cfb955d4dca121a539e7b65e4c367364fc57d
Maxim <godlikegondar@gmail.com>
2023-02-09 06:56:39 +0300

    Enhancements
    Cookie fixed, meaning of the text added, buttons now clickable, external links now open in a new window, notifications now adapted too.

--- js/script.js
+++ js/script.js
@@ -20,15 +20,15 @@ const routes = {
 };
 
 const locationHandler = async () => {
-  var location = window.location.hash.replace("#", "");
-  if (location.length == 0) {
+  let location = window.location.hash.replace("#", "");
+  if (location.length === 0) {
     location = "/";
   }
   const route = routes[location] || routes["404"];
   const html = await fetch(route.template).then((response) => response.text());
   document.querySelector("main").innerHTML = html;
   document.title = route.title;
-  if (location == "portfolio") {
+  if (location === "portfolio") {
     makeSlider();
   }
 };
@@ -38,34 +38,27 @@ locationHandler();
 
 //modal
 
-var modal = document.querySelector(".modal");
-var contactButton = document.querySelectorAll(".contact-button");
-var closeModal = document.querySelector(".close-modal");
-let submitButton = document.querySelector(".submit-button");
+let modal = document.querySelector(".modal"),
+  contactButton = document.querySelectorAll(".contact-button"),
+  closeModal = document.querySelector(".close-modal"),
+  submitButton = document.querySelector(".submit-button"),
+  newsButton = document.querySelector(".continue");
 
-let name = document.forms.contactForm[0];
-let surname = document.forms.contactForm[1];
-let email = document.forms.contactForm[2];
-let phone = document.forms.contactForm[3];
+let name = document.forms.contactForm[0],
+  surname = document.forms.contactForm[1],
+  email = document.forms.contactForm[2],
+  phone = document.forms.contactForm[3];
 
+let forms = document.forms[0];
 
-contactButton[0].onclick = function () {
-  modal.style.display = "block";
-  if (!document.cookie == "") {
-    name.value = document.cookie.split(",")[0];
-    surname.value = document.cookie.split(",")[1];
-    email.value = document.cookie.split(",")[2];
-    phone.value = document.cookie.split(",")[3];
-  }
-}
-
-contactButton[1].onclick = function () {
-  modal.style.display = "block";
-  if (!document.cookie == "") {
-    name.value = document.cookie.split(",")[0];
-    surname.value = document.cookie.split(",")[1];
-    email.value = document.cookie.split(",")[2];
-    phone.value = document.cookie.split(",")[3];
+for (let i = 0; i < contactButton.length; i++) {
+  contactButton[i].onclick = function () {
+    modal.style.display = "block";
+    if (document.cookie !== "") {
+      for (let i = 0; i < forms.length; i++) {
+        forms[i].value = document.cookie.split(";")[i].trim().split("=")[1];
+      }
+    }
   }
 }
 
@@ -76,49 +69,65 @@ closeModal.onclick = function () {
 submitButton.onclick = function () {
   let status = true;
 
+  testForm(name, testName);
+  testForm(surname, testName);
+  testForm(email, testEmail);
+  testForm(phone, testPhone);
 
-  if (!testName(name.value)) {
-    status = false;
-    name.classList.add("wrong-input");
-  } else {
-    name.classList.remove("wrong-input");
-  }
-
-  if (!testName(surname.value)) {
-    status = false;
-    surname.classList.add("wrong-input");
-  } else {
-    surname.classList.remove("wrong-input");
-  }
-
-  if (!testEmail(email.value)) {
-    status = false;
-    email.classList.add("wrong-input");
-  } else {
-    email.classList.remove("wrong-input");
-  }
-
-  if (!testPhone(phone.value)) {
-    status = false;
-    phone.classList.add("wrong-input");
-  } else {
-    phone.classList.remove("wrong-input");
+  function testForm(formName, testFormFunc) {
+    if (!testFormFunc(formName.value)) {
+      status = false;
+      formName.classList.add("wrong-input");
+    } else {
+      formName.classList.remove("wrong-input");
+    }
   }
 
   if (status) {
+    if (document.cookie !== "") {
+      cookieManage("name", -1);
+      cookieManage("surname", -1);
+      cookieManage("email", -1);
+      cookieManage("phone", -1);
+    }
     modal.style.display = "none";
-    document.cookie = `${name.value},${surname.value},${email.value},${phone.value}; max-age=10000`;
+    cookieManage("name", 10000);
+    cookieManage("surname", 10000);
+    cookieManage("email", 10000);
+    cookieManage("phone", 10000);
+
     document.querySelector(".call-back").style.display = "flex";
     setTimeout(notificationNone, 2900);
   }
 }
 
+newsButton.onclick = function () { 
+  let emailInput = document.querySelector(".stay-in-loop-email input");
+  if (testEmail(emailInput.value)) {
+    document.querySelector(".news").style.display = "flex";
+    emailInput.value = "";
+    emailInput.classList.remove("wrong-email");
+    setTimeout(notificationNewsNone, 2900);
+  }
+  else {
+    emailInput.classList.add("wrong-email");
+  }
+}
+
+function cookieManage(formName, age) {
+  document.cookie = `${formName}=${eval(formName).value}; max-age=${age}`;
+}
+
 function notificationNone() {
-  document.querySelector(".call-back").style.display = "none"
+  document.querySelector(".call-back").style.display = "none";
+}
+
+function notificationNewsNone() {
+  document.querySelector(".news").style.display = "none";
 }
 
 window.onclick = function (event) {
-  if (event.target == modal) {
+  if (event.target === modal) {
     modal.style.display = "none";
   }
 }
@@ -150,10 +159,12 @@ getData = async function () {
 function makeSlider() {
   let fakeDataObject = JSON.parse(window.localStorage.getItem('fakeData'));
 
-  let comments = document.querySelectorAll(".portfolio-comment");
-  let commentNames = document.querySelectorAll(".comment-name");
-  let commentEmails = document.querySelectorAll(".comment-email");
-  let commentBodies = document.querySelectorAll(".comment-body");
+  let gap,
+    commentWidth,
+    comments = document.querySelectorAll(".portfolio-comment"),
+    commentNames = document.querySelectorAll(".comment-name"),
+    commentEmails = document.querySelectorAll(".comment-email"),
+    commentBodies = document.querySelectorAll(".comment-body");
 
   for (let i = 0; i < comments.length; i++) {
     let name = fakeDataObject[i].name.split(' ')[0];
@@ -167,29 +178,34 @@ function makeSlider() {
     commentBodies[i].innerHTML = fakeDataObject[i].body;
   }
 
-
   let offset = 0;
   document.querySelector(".next-button").addEventListener("click", function () {
-    let gap = comments[1].offsetLeft - comments[0].offsetLeft - comments[0].offsetWidth;
-    let commentWidth = comments[0].offsetWidth;
-
+    CalcSliderGap();
     offset += commentWidth + gap;
     if (offset > (commentWidth + gap) * 3) {
       offset = 0;
     }
-    document.querySelector(".portfolio-comment-div").style.left = - offset + "px";
+    CalcSliderOffset();
   });
 
   document.querySelector(".prev-button").addEventListener("click", function () {
-    let gap = comments[1].offsetLeft - comments[0].offsetLeft - comments[0].offsetWidth;
-    let commentWidth = comments[0].offsetWidth;
-
+    CalcSliderGap();
     offset -= commentWidth + gap;
     if (offset < 0) {
       offset = (commentWidth + gap) * 3;
     }
-    document.querySelector(".portfolio-comment-div").style.left = - offset + "px";
+    CalcSliderOffset();
   });
+
+  function CalcSliderGap() {
+    gap = comments[1].offsetLeft - comments[0].offsetLeft - comments[0].offsetWidth;
+    commentWidth = comments[0].offsetWidth;
+    return { gap, commentWidth };
+  }
+
+  function CalcSliderOffset() {
+    document.querySelector(".portfolio-comment-div").style.left = - offset + "px";
+  }
 }
 
 //burger
@@ -202,7 +218,7 @@ burgerIcon.onclick = function () {
   burgerBody.classList.toggle('display-burger');
 }
 
-for (i = 0; i < burgerList.length; i++) {
+for (let i = 0; i < burgerList.length; i++) {
   burgerList[i].onclick = function () {
     burgerBody.classList.toggle('display-burger');
   }
